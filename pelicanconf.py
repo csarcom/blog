739998709e90c87ec707096b1d6925156167b208
Charles Sartori <charles.sartori@gmail.com>
2015-12-18 19:45:34 -0200

    Change general configs

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 AUTHOR = 'Charles Sartori'
-SITENAME = 'Just one more Blog!'
+SITENAME = 'Charles Sartori'
 SITEURL = 'http://charlessartori.com'
 
 PATH = 'content'
@@ -19,15 +19,11 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('github', 'https://github.com/csarcom'),
+          ('facebook', 'https://www.facebook.com/charlesmpsartori'),
+          ('linkedin', 'https://br.linkedin.com/in/charlessartori'))
 
 DEFAULT_PAGINATION = 3
 
